e264f290a37c1a562280ae56b64f18e3c7c61712
Sukesh Reddy Rondla <65500920+SukeshRondla@users.noreply.github.com>
2024-02-21 13:53:31 -0600

    Add files via upload

---
+++ Hike/Hike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Sukesh Reddy on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1").resizable().scaledToFit()
+        
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Sukesh Reddy on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
